0244b10982f1e6082f293e950d1b840c504a145a
zalven-official <zalvendayao888@gmail.com>
2022-09-27 16:52:09 +0800

    adding video camera

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,39 @@
+use js_sys::Boolean;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{window, HtmlVideoElement, MediaStream, MediaStreamConstraints};
 use yew::prelude::*;
 
 #[function_component(Producer)]
 fn producer() -> Html {
+    wasm_bindgen_futures::spawn_local(async move {
+        let navigator = window().unwrap().navigator();
+        let media_devices = navigator.media_devices().unwrap();
+        let video_element = window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id("webcam")
+            .unwrap()
+            .unchecked_into::<HtmlVideoElement>();
+        let mut constraints = MediaStreamConstraints::new();
+        constraints.video(&Boolean::from(true));
+        let devices_query = media_devices
+            .get_user_media_with_constraints(&constraints)
+            .unwrap();
+
+        let device = JsFuture::from(devices_query)
+            .await
+            .unwrap()
+            .unchecked_into::<MediaStream>();
+
+        video_element.set_src_object(Some(&device));
+    });
+
     html!(
         <div class={"bg-gradient-to-r from-green-400 to-blue-500 p-10 font-mono"}>
             <h3 class={"bg-white text-center p-5 rounded-lg text-blue shadow-lg "}>{"Producer"}</h3>
+            <video autoplay=true id="webcam"></video>
         </div>
     )
 }
